e6494da45379987466cecd53fd1350fc45a8f5e5
Guilherme <guilherme@dgsistemas.com.br>
2021-03-10 18:25:55 -0300

    att

--- kzas-app/react/crud/frontend/src/components/home/Home.jsx
+++ kzas-app/react/crud/frontend/src/components/home/Home.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Main from '../template/Main'
-
+import Cadastro from '../user/userCrud'
 
 export default props =>
 
 
-
-
-<Main icon='user' title='Cadastro Usuários'
+ <Main icon='user' title='Cadastro Usuários'
         subtitle='Teste de Front-End Kzas'
     >
+
         <div className='display-4'>Bem Vindo!</div>
         <hr />
         <p className='mb-0'>Sistema teste desenvolvido para
